f96450381337fb4dbfc7e4d18a5797fcf9c7409d
Muhammed AbdElrhman <ma.prog.zero@gmail.com>
2020-05-05 01:51:17 +0200

    initial commit

---
+++ 1.4.py
@@ -0,0 +1,15 @@
+import os,shutil
+filePath=input("enter path to rib : ")
+fileFormate=input("enter files formate : ")
+fileSavePath=input("enter save path : ")
+
+if os.path.exists(filePath):
+    if "." in fileFormate:
+        fileFormate=fileFormate-"."
+    while True:
+        for Root,Folders,Files in os.walk(filePath):
+            for file in Files:
+	            if file.lower().split(".")[-1]==fileFormate:
+		        shutil.copy(os.path.join(Root,file),fileSavePath)
+else:
+    print("path don't exsist")
